1078f6b29ea530dbf024657fca8b8302d3174dd7
SravanthMolabanti <molabantisravanth@gmail.com>
2023-10-14 18:39:25 +0530

    first commit

---
+++ src/components/Education.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Education = () => {
+  return (
+    <section>
+                <h2>Education</h2>
+
+                <h3>Vellore Institute of Technology, Vellore</h3>
+                <p>B.tech (Computer Science Engineering)</p>
+                <p>Graduation Year: 2025</p>
+
+                <h3>Dr. KKR. Happy Valley School, Agiripalli</h3>
+                <p>Intermediate</p>
+                <p>Graduation Year: 2021</p>
+
+                <h3>Dr. KKR. Gowtham International School, Vijayawada</h3>
+                <p>10th standard</p>
+                <p>Graduation Year: 2019</p>
+            
+            
+    </section>
+  );
+}
+
+export default Education;
+
